3deaa5e2c9714acd547b0a341bdd9e34413be1c9
Foster Z <fosterunlimited0123@gmail.com>
2024-07-06 23:44:36 +0530

    fix:[configurations]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	darkMode: 'class',
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				accent: {
+					primary: "var(--primary-accent) / <alpha-value>",
+					secondary: "var(--secondary-accent) / <alpha-value>",
+				},
+				fg: "var(--background-color) / <alpha-value>",
+				bg: "var(--foreground-color) / <alpha-value>",
+				sc: "var(--surface-color) / <alpha-value>",
+			}
+		},
+	},
+	plugins: [],
 }
