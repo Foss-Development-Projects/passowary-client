334beae11d59110f8c55fa55b27c3fa8b12645aa
Foster Z <fosterunlimited0123@gmail.com>
2024-07-07 00:08:32 +0530

    fix:[add_more_router]

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import './App.scss'
 import HomePage from './pages/home/Home.page';
 import AccountPage from './pages/account/Account.page';
 import AccountProfile from './components/account/profile/AccountProfile.component';
+import RegisterPage from './pages/register/Register.page';
+import LoginPage from './pages/login/Login.page';
+import ResetPasswordPage from './pages/reset-password/ResetPassword.page';
+import ForgotPasswordPage from './pages/forgot-password/ForgotPassword.page';
+import AboutPage from './pages/about/About.page';
 
 function App() {
 
@@ -19,6 +24,11 @@ function App() {
                     <Route path='' element={<AccountProfile />} />
                     <Route path='profile' element={<AccountProfile />} />
                 </Route>
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/register' element={<RegisterPage />} />
+                <Route path='/forgot-password' element={<ForgotPasswordPage />} />
+                <Route path='/reset-password' element={<ResetPasswordPage />} />
+                <Route path='/about' element={<AboutPage />} />
             </Routes>
         </BrowserRouter>
         
