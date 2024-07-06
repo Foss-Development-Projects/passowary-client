f12294e4c6b5c2b530cb010e7b1cd46d799c63c4
Foster Z <fosterunlimited0123@gmail.com>
2024-07-06 20:13:51 +0530

    feat:[navlink]

---
+++ src/modules/navlink/Navlink.module.tsx
@@ -0,0 +1,23 @@
+// Libraries & Packages
+
+import { Link } from 'react-router-dom';
+
+// Stylesheets
+
+import './navlink.module.scss';
+
+// Component
+const Navlink = (props: any) => {
+    return (
+        <li className="h-9 w-full p-0.5">
+            <Link
+                className="transition-colors font-bold text-lg flex items-center justify-center px-3 mx-2 h-full rounded-full hover:navbar-shade"
+                to={props.href}
+            >
+                {props.icon}
+                <p className="mx-1">{props.name}</p>
+            </Link>
+        </li>
+    )
+}
+export default Navlink;
