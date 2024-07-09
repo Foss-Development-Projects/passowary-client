0f5f4274228b3cf49b8a0c52d04c08754cdc69cb
Foster Z <fosterunlimited0123@gmail.com>
2024-07-09 14:18:40 +0530

    feat: [add_delete_component_to_account_page]

---
+++ src/components/account/danger/AccountDanger.component.tsx
@@ -0,0 +1,34 @@
+import { BiTrash } from "react-icons/bi";
+import { GrHalt } from "react-icons/gr";
+
+const AccountDanger = () => {
+    const iconSize = 24;
+    return (
+        <div>
+            <h1 className="text-center font-bold text-2xl">Danger</h1>
+            <form action="" name="account-delete-form" className="p-2 rounded-md" method="POST">
+                <section className="bg-zinc-950/[.09] p-2 rounded-md my-3">
+                    <section className="flex items-center justify-between">
+                        <p className="text-md">Suspend Your Acccount For A Period of Time</p>
+                        <button type="submit" title="Suspend Account Temporarily" className="bg-yellow-600 
+                    hover:bg-yellow-700 ml-3 p-2 rounded-full text-gray-200"><GrHalt size={iconSize} /></button>
+                    </section>
+                    <section className="my-2">
+                        <p className="text-pretty text-sm">Temporarily disables your account for a certain period of time. You can activate your account at anytime</p>
+                    </section>
+                </section>
+                <section className="bg-zinc-950/[.09] p-2 rounded-md my-3">
+                    <section className="flex items-center justify-between">
+                        <p className="text-md">Delete Your Acccount Permanently</p>
+                        <button type="submit" title="Delete Account Permanently" className="bg-red-600 
+                    hover:bg-red-700 ml-3 p-2 rounded-full text-gray-200"><GrHalt size={iconSize} /></button>
+                    </section>
+                    <section className="my-2">
+                        <p className="text-pretty text-sm">Permanently deletes your account along with all your data. Please proceed with cautiion.</p>
+                    </section>
+                </section>
+            </form>
+        </div>
+    )
+}
+export default AccountDanger;
